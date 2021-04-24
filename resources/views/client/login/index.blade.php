cc7b76b03ff241e742b0430419480ab91ef4e91c
Mtt-linh <maithuylinh2196@gmail.com>
2021-04-24 14:23:59 +0700

    text commit login

--- resources/views/client/login/index.blade.php
+++ resources/views/client/login/index.blade.php
@@ -7,12 +7,12 @@
     <div class="container">
         <div class="login">
             <h1 class="login-title">HARA PETA Project</h1>
-            <p class="login-desc">会員ログイン</p>
+            <p class="login-desc">Member Login</p>
 
             <form method="POST" action="{{ route('post.login') }}" class="form-login">
                 @csrf
                 <div class="form-row">
-                    <label for="" class="form-label">カロママプラスID</label>
+                    <label for="" class="form-label">Caromama Plus ID</label>
                     <div class="form-input">
                         <input type="text" name="code">
                     </div>
@@ -20,11 +20,11 @@
                 <div class="form-row">
                     <label for="" class="form-label"></label>
                     <div class="form-input">
-                        <a href="{{ route('search') }}" class="note">カロママプラスIDが不明な方</a>
+                        <a href="{{ route('search') }}" class="note">Those who do not know the Caromama Plus ID</a>
                     </div>
                 </div>
                 <div class="form-row">
-                    <label for="" class="form-label">パスワード</label>
+                    <label for="" class="form-label">Password</label>
                     <div class="form-input">
                         <input type="password" name="password">
                     </div>
@@ -34,7 +34,7 @@
                         <div class="error"> {!! $errors->first() !!} </div>
                     @endif
                 </div>
-                <button type="submit" class="button">ログインする</button>
+                <button type="submit" class="button">Login</button>
             </form>
         </div>
     </div>
