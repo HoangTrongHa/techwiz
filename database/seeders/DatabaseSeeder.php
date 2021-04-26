eb473ef5083da44a6f4cddfe230ba7e55c5f9fb8
ttnghia24501 <trantuannghia24501@gmail.com>
2021-04-26 14:24:20 +0700

    categoryseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,31 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call('Category');
-    }
-}
-class Category extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-       DB::table('categories')->insert([
-           [
-               'name' => 'Sweaty Bodyweight Cardio Butt and Thigh Workout',
-                'rules' => '1'
-           ],
-           [
-            'name' => '20 Minute Butt and Thigh Workout - Glute Activation Workout',
-             'rules' => '1'
-            ],
-            [
-                'name' => 'Upper Body Workout to Build Strength for Push Ups + Tips on How to Do More Push Ups',
-                'rules' => '1'
-            ]
-       ]);
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class
+        ]);
     }
 }
